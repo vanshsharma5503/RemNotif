0c83ebd89b47cebf99e608bf1de06d8e1f55acd9
vansh sharma <vb2810@srmist.edu.in>
2024-03-11 13:06:13 +0530

    Initial Commit

---
+++ RemNotif/RemNotifApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemNotifApp.swift
+//  RemNotif
+//
+//  Created by SHHH!! private on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RemNotifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
